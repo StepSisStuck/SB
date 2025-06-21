51d173b8edcd8fcddeede188db5b7256cb4cbbf4
Justclf <chanluofeng@hotmail.com>
2025-06-22 01:41:54 +0800

    q8

--- src/routes/reportRoutes.js
+++ src/routes/reportRoutes.js
@@ -5,6 +5,7 @@ const controller = require('../controllers/reportController');
 // router.post("/", controller.CheckUserId, controller.CheckVulnerabilityId, controller.CheckReportExists, controller.CreateNewReport, controller.UpdateUserReputation) // Verify if the UserId exists, followed by vulnID. Then create report record and link users and vuln
 router.post("/", controller.CreateReport)
 
+router.put("/:id", controller.CheckReport, controller.CheckCloser, controller.FinishReport)
 
 
 module.exports = router;
