e12bcfabbd979b9a818e6712098ed3b195536d61
Justclf <chanluofeng@hotmail.com>
2025-06-22 19:02:04 +0800

    Question 7 done properly

--- src/controllers/reportController.js
+++ src/controllers/reportController.js
@@ -1,67 +1,143 @@
 const model = require("../models/reportModel.js");
-const { report } = require("../routes/userRoutes.js");
+
 
 
 // Question 7
 
 
-module.exports.CreateReport = (req, res, next) => {
-    const { user_id, vulnerability_id } = req.body;
-
-    if (!user_id || !vulnerability_id) {
-        return res.status(400).send("User_id and Vulnerability_id is required.");
+// Verify that user exists
+module.exports.CheckUserId = (req, res, next) =>
+{
+    if (req.body.user_id == undefined){
+            return res.status(400).send("User_id is not complete")
     }
 
-// Verify that the userId exists
-    model.GetUserById(user_id, (error, results) => {
-        if (error) {
+    const data = {
+        id: req.body.user_id
+    }
+
+    const callback = (error, results, fields) => {
+        if (error){
             console.error("Error CheckUserId:", error);
             return res.status(500).json(error);
         } else {
-            if (results.length === 0) {
-                return res.status(404).json({ message: "User not found" });
+            if (results.length === 0){
+                return res.status(404).json({message: "User not found"})
+            } else {
+                res.locals.user = results[0];
+                next();
             }
         }
-        const user = results[0];
+    }
+    model.GetUserById(data, callback);
+}
 
-// Verify that the Vulnerability exists
-    model.GetVulnerabilityById(vulnerability_id, (error, results) => {
-        if (error) {
+
+// Verify vulnerability exists
+module.exports.CheckVulnerabilityId = (req, res, next) =>
+{
+    if (req.body.vulnerability_id == undefined){
+        return res.status(400).send("Vulnerability_id is not complete")
+    }
+
+    const data = {
+        id: req.body.vulnerability_id
+    }
+    
+    const callback = (error, results, fields) => {
+        if (error){
             console.error("Error CheckVulnerabilityId:", error);
             return res.status(500).json(error);
         } else {
-            if (results.length === 0) {
-                return res.status(404).json({ message: "Vulnerability not found" });
+            if (results.length === 0){
+                return res.status(404).json({message: "Vulnerability not found"})
+            } else {
+                res.locals.vulnerability = results[0];
+                next();
             }
         }
-        const vuln = results[0];
+    }
+    model.GetVulnerabilityById(data, callback);
+}
 
-// Setting up the requirement to key in to the report
-    model.CreateNewReport(user_id, vulnerability_id, (error, results) => {
-        if (error) {
-            console.error("Error CheckReportExists:", error);
+// // Verify if the report exists
+// module.exports.CheckReportExists = (req, res, next) => {
+//     const data = {
+//         user_id: req.body.user_id,
+//         vulnerability_id: req.body.vulnerability_id
+//     };
+// 
+//     const callback = (error, results, fields) => {
+//         if (error) {
+//             console.error("Error CheckReportExists:", error);
+//             return res.status(500).json(error);
+//         }
+// 
+// 
+// // Essentialy saying that if i created a POST, it becomes 1, hence its not lesser than 0, so it'll show the info only.
+//         if (results.length > 0) {
+//             return res.status(200).json({
+//                 id: results[0].id,
+//                 user_id: req.body.user_id,
+//                 vulnerability_id: req.body.vulnerability_id,
+//                 status: results[0].status,
+//                 user_reputation: res.locals.user.reputation
+//             })
+//         }
+//         next();
+//     }
+// 
+//     model.CheckReportExists(data, callback)
+// }
+
+
+
+// Create report
+module.exports.CreateNewReport = (req, res, next) =>
+{
+    const data = {
+        user_id: req.body.user_id,
+        vulnerability_id: req.body.vulnerability_id
+    }
+
+    const callback = (error, results, fields) => {
+        if (error){
+            console.error("Error creating report:", error);
             return res.status(500).json(error);
-            }
-            const reportId = results.insertId;
-            const newRep = user.reputation + vuln.points;
+        }
+        res.locals.reportId = results.insertId;
+        next();
+    }
+    model.CreateReport(data, callback);
+}
 
-// Add the reputation and display the final output
-        model.UpdateReputation(user, newRep, (error, results) => {
-            if (error) {
-                console.error("Error CheckReportExists:", error);
-                return res.status(500).json(error);
-                }
-                res.status(201).json({
-                    id: reportId, 
-                    user_id,
-                    vulnerability_id,
-                    status: 0,
-                    user_reputation: newRep
-                });
-                });
-            });
-        });
-    });
+
+// Update reputation
+module.exports.UpdateUserReputation = (req, res) =>
+{
+    // Combining the reputation
+    const newRep = res.locals.user.reputation + res.locals.vulnerability.points // user.reputation means that its getting its stuff in user//reputation
+
+    const data = {
+        id: res.locals.user.id,
+        reputation: newRep
+    }
+
+    const callback = (error, results, fields) => {
+        if (error) {
+            console.error("Error updating reputation:", error)
+            return res.status(500).json(error)
+        }
+        res.status(201).json({
+            id: res.locals.reportId,
+            user_id: req.body.user_id,
+            vulnerability_id: req.body.vulnerability_id,
+            status: 0,
+            user_reputation: res.locals.vulnerability.points
+        })
+    }
+
+    model.UpdateReputation(data, callback)
 }
 
 
@@ -70,132 +146,132 @@ module.exports.CreateReport = (req, res, next) => {
 
 
 // Verify that the report exists
-module.exports.CheckReport = (req, res, next) => {
-    if (req.body.status === undefined || req.body.user_id == undefined) {
-        return res.status(400).json({message: "Error: Status or User_id is undefined"})
-    }
-
-    const data = {
-        id: req.params.id,
-    }
-
-    const callback = (error, results, fields) => {
-        if (error) {
-            console.error("Error CheckReport:", error);
-            return res.status(500).json(error);
-        }
-        if (results.length === 0) {
-            return res.status(404).json({message: "Report not found"})     
-        }
-        res.locals.report = results[0]
-        next()
-    }
-
-    model.CheckReportById(data, callback)
-}
-
-
-// Verify the user_id(Closer) exists
-module.exports.CheckCloser = (req, res, next) => {
-    const data = {
-        id: req.body.user_id
-    }
-
-    const callback = (error, results, fields) => {
-        if (error) {
-            console.error("Error fetching user:", error)
-            return res.status(500).json(error);
-        }
-        if (results.length === 0) {
-            return res.status(404).json({message: "User not found"})
-        }
-
-        res.locals.user = results[0]
-        next()
-    }
-
-    model.CheckUserById(data, callback)
-}
-
-
-// Update report
-module.exports.FinishReport = (req, res, next) => {
-    
-    const data = {
-        id: req.params.id,
-        status: req.body.status,
-        user_id: req.body.user_id
-    }
-
-    const callback = (error, results, fields) => {
-        if (error) {
-            console.error("Error UpdateReport:", error);
-            return res.status(500).json(error);
-        }
-
-        res.status(200).json({
-            id: Number(data.id), // Number makes it int
-            status: data.status,
-            closer_id: data.user_id,
-            user_reputation: results.newRep
-        });
-    }
-
-    model.UpdateReportAndReputation(data, callback);
-}
-
-
-
-
-module.exports.UpdateReport = (req, res, next) => {
-    const { id } = req.params;
-    const { status, user_id } = req.body;
-
-    if (!status || !user_id) {
-        return res.status(400).send("Status and User_id is required.");
-    }
-
-    model.CheckReportById(id, (error, results) => {
-        if (error) {
-            console.error("Error CheckReportById:", error);
-            return res.status(500).json(error);
-        }  else {
-            if (results.length === 0) {
-            return res.status(404).json({ message: "Report not found" });
-        }
-        }
-    })
-
-
-    model.CheckUserById(user_id, (error, results) => {
-        if (error) {
-            console.error("Error CheckUserById:", error)
-            return res.status(500).json(error);
-        } else {
-            if (results.length === 0) {
-            return res.status(404).json({message: "User not found"})
-        }
-        }
-
-    })
-
-
-    model.UpdateReportAndReputation(id, status, user_id, (error, results) => {
-        if (error) {
-            console.error("Error UpdateReport:", error);
-            return res.status(500).json(error);
-        }
-        res.status(200).json({
-            id: Number(id), // Number makes it int
-            status: data.status,
-            closer_id: data.user_id,
-            user_reputation: results.newRep
-        })
-
-    })
-
-}
-
-
-    
-
+// module.exports.CheckReport = (req, res, next) => {
+//     if (req.body.status === undefined || req.body.user_id == undefined) {
+//         return res.status(400).json({message: "Error: Status or User_id is undefined"})
+//     }
+// 
+//     const data = {
+//         id: req.params.id,
+//     }
+// 
+//     const callback = (error, results, fields) => {
+//         if (error) {
+//             console.error("Error CheckReport:", error);
+//             return res.status(500).json(error);
+//         }
+//         if (results.length === 0) {
+//             return res.status(404).json({message: "Report not found"})     
+//         }
+//         res.locals.report = results[0]
+//         next()
+//     }
+// 
+//     model.CheckReportById(data, callback)
+// }
+// 
+// 
+// // Verify the user_id(Closer) exists
+// module.exports.CheckCloser = (req, res, next) => {
+//     const data = {
+//         id: req.body.user_id
+//     }
+// 
+//     const callback = (error, results, fields) => {
+//         if (error) {
+//             console.error("Error fetching user:", error)
+//             return res.status(500).json(error);
+//         }
+//         if (results.length === 0) {
+//             return res.status(404).json({message: "User not found"})
+//         }
+// 
+//         res.locals.user = results[0]
+//         next()
+//     }
+// 
+//     model.CheckUserById(data, callback)
+// }
+// 
+// 
+// // Update report
+// module.exports.FinishReport = (req, res, next) => {
+//     
+//     const data = {
+//         id: req.params.id,
+//         status: req.body.status,
+//         user_id: req.body.user_id
+//     }
+// 
+//     const callback = (error, results, fields) => {
+//         if (error) {
+//             console.error("Error UpdateReport:", error);
+//             return res.status(500).json(error);
+//         }
+// 
+//         res.status(200).json({
+//             id: Number(data.id), // Number makes it int
+//             status: data.status,
+//             closer_id: data.user_id,
+//             user_reputation: results.newRep
+//         });
+//     }
+// 
+//     model.UpdateReportAndReputation(data, callback);
+// }
+// 
+// 
+// 
+// 
+// module.exports.UpdateReport = (req, res, next) => {
+//     const { id } = req.params;
+//     const { status, user_id } = req.body;
+// 
+//     if (!status || !user_id) {
+//         return res.status(400).send("Status and User_id is required.");
+//     }
+// 
+//     model.CheckReportById(id, (error, results) => {
+//         if (error) {
+//             console.error("Error CheckReportById:", error);
+//             return res.status(500).json(error);
+//         }  else {
+//             if (results.length === 0) {
+//             return res.status(404).json({ message: "Report not found" });
+//         }
+//         }
+//     })
+// 
+// 
+//     model.CheckUserById(user_id, (error, results) => {
+//         if (error) {
+//             console.error("Error CheckUserById:", error)
+//             return res.status(500).json(error);
+//         } else {
+//             if (results.length === 0) {
+//             return res.status(404).json({message: "User not found"})
+//         }
+//         }
+// 
+//     })
+// 
+// 
+//     model.UpdateReportAndReputation(id, status, user_id, (error, results) => {
+//         if (error) {
+//             console.error("Error UpdateReport:", error);
+//             return res.status(500).json(error);
+//         }
+//         res.status(200).json({
+//             id: Number(id), // Number makes it int
+//             status: data.status,
+//             closer_id: data.user_id,
+//             user_reputation: results.newRep
+//         })
+// 
+//     })
+// 
+// }
+// 
+// 
+//     
+// 
--- src/models/reportModel.js
+++ src/models/reportModel.js
@@ -5,126 +5,143 @@ const pool = require('../services/db');
 
 
 
-module.exports.GetUserById = (user_id, callback) =>
+module.exports.GetUserById = (data, callback) =>
 {
     const SQLSTATEMENT = `
     SELECT id, reputation
     FROM user
     WHERE id = ?
     `
+    const VALUES = [data.id]
 
-    pool.query(SQLSTATEMENT, [user_id], callback)
+    pool.query(SQLSTATEMENT, VALUES, callback)
 }
 
 
-module.exports.GetVulnerabilityById = (vulnerability_id, callback) =>
+module.exports.GetVulnerabilityById = (data, callback) =>
 {
     const SQLSTATEMENT = `
     SELECT id, points
     FROM vulnerability
     WHERE id = ?
     `
+    const VALUES = [data.id]
 
-    pool.query(SQLSTATEMENT, [vulnerability_id], callback)
+    pool.query(SQLSTATEMENT, VALUES, callback)
 }
 
+// module.exports.CheckReportExists = (data, callback) =>
+// {
+//     const SQL = `
+//         SELECT id, status
+//         FROM report
+//         WHERE user_id = ?
+//         AND vulnerability_id = ?
+//     `
+//     const VALUES = [ data.user_id, data.vulnerability_id ]
+//     pool.query(SQL, VALUES, callback)
+// }
 
-module.exports.CreateNewReport = (user_id, vulnerability_id, callback) => 
+
+module.exports.CreateReport = (data, callback) =>
 {
     const SQLSTATEMENT = `
     INSERT INTO report (user_id, vulnerability_id)
-    VALUES (?, ?);
-    `;
+    VALUES (?, ?)
+    `
+    const VALUES = [data.user_id, data.vulnerability_id]
 
-    pool.query(SQLSTATEMENT, [user_id, vulnerability_id], callback)
+    pool.query(SQLSTATEMENT, VALUES, callback)
 }
 
-module.exports.UpdateReputation = (user, newRep, callback) =>
+
+module.exports.UpdateReputation = (data, callback) =>
 {
     const SQLSTATEMENT = `
     UPDATE user
     SET reputation = ?
     WHERE id = ?
-    `;
+    `
+    const VALUES = [data.reputation, data.id]
 
-    pool.query(SQLSTATEMENT, [user, newRep], callback)
+    pool.query(SQLSTATEMENT, VALUES, callback)
 }
 
 
 
 
 // Question 8
-module.exports.CheckReportById = (data, callback) => {
-    const SQLSTATEMENT = `
-    SELECT id
-    FROM report
-    WHERE id = ?;
-    `;
-    const VALUES = [data.id];
-
-    pool.query(SQLSTATEMENT, VALUES, callback);
-}
-
-module.exports.CheckUserById = (data, callback) => {
-    const SQLSTATEMENT = `
-    SELECT id 
-    FROM user
-    WHERE id = ?
-    `;
-    const VALUES = [data.id];
-
-    pool.query(SQLSTATEMENT, VALUES, callback);
-}
-
-module.exports.UpdateReportAndReputation = (data, callback) =>
-{
-    // const SQLSTATEMENT = `
-    //     /* update report and user rep */
-    //     UPDATE report AS r
-    //     JOIN vulnerability AS v ON v.id = r.vulnerability_id
-    //     JOIN user AS u ON u.id = ?
-    //     SET r.status     = ?,
-    //     r.user_id = ?,
-    //            u.reputation = CASE
-    //                            WHEN r.status <> ?
-    //                            THEN u.reputation + v.points
-    //                            ELSE u.reputation
-    //                           END
-    //     WHERE r.id = ?;
-    //     SELECT u.reputation AS user_reputation
-    //     FROM user u
-    //     WHERE u.id = ?;
-    // `;
-    const SQLSTATEMENT = `
-    UPDATE report
-    JOIN vulnerability ON vulnerability.id = report.vulnerability_id
-    JOIN user ON user.id = ?
-    SET
-    report.status = ?,
-    report.user_id = ?,
-    user.reputation = CASE
-        WHEN report.status <> ? 
-        THEN user.reputation + vulnerability.points
-        ELSE user.reputation
-    END
-    Where report.id = ?;
-    
-    SELECT reputation AS user_reputation 
-    FROM user
-    WHERE id = ?;
-    `;
-    // Update the report status and user's reputation
-
-    const VALUES = [data.user_id, data.status, data.user_id, data.status, data.id, data.user_id];
-
-    pool.query(SQLSTATEMENT, VALUES, (error, results) =>
-    {
-        if (error) {
-            return callback(error);
-        }
-
-        // results[0] is UPDATE OkPacket, results[1] is SELECT rows
-        const newRep = results[1][0].user_reputation;
-        callback(null, { affectedRows: results[0].affectedRows, newRep });
-    });
-};
+// module.exports.CheckReportById = (data, callback) => {
+//     const SQLSTATEMENT = `
+//     SELECT id
+//     FROM report
+//     WHERE id = ?;
+//     `;
+//     const VALUES = [data.id];
+// 
+//     pool.query(SQLSTATEMENT, VALUES, callback);
+// }
+// 
+// module.exports.CheckUserById = (data, callback) => {
+//     const SQLSTATEMENT = `
+//     SELECT id 
+//     FROM user
+//     WHERE id = ?
+//     `;
+//     const VALUES = [data.id];
+// 
+//     pool.query(SQLSTATEMENT, VALUES, callback);
+// }
+// 
+// module.exports.UpdateReportAndReputation = (data, callback) =>
+// {
+//     // const SQLSTATEMENT = `
+//     //     /* update report and user rep */
+//     //     UPDATE report AS r
+//     //     JOIN vulnerability AS v ON v.id = r.vulnerability_id
+//     //     JOIN user AS u ON u.id = ?
+//     //     SET r.status     = ?,
+//     //     r.user_id = ?,
+//     //            u.reputation = CASE
+//     //                            WHEN r.status <> ?
+//     //                            THEN u.reputation + v.points
+//     //                            ELSE u.reputation
+//     //                           END
+//     //     WHERE r.id = ?;
+//     //     SELECT u.reputation AS user_reputation
+//     //     FROM user u
+//     //     WHERE u.id = ?;
+//     // `;
+//     const SQLSTATEMENT = `
+//     UPDATE report
+//     JOIN vulnerability ON vulnerability.id = report.vulnerability_id
+//     JOIN user ON user.id = ?
+//     SET
+//     report.status = ?,
+//     report.user_id = ?,
+//     user.reputation = CASE
+//         WHEN report.status <> ? 
+//         THEN user.reputation + vulnerability.points
+//         ELSE user.reputation
+//     END
+//     Where report.id = ?;
+//     
+//     SELECT reputation AS user_reputation 
+//     FROM user
+//     WHERE id = ?;
+//     `;
+//     // Update the report status and user's reputation
+// 
+//     const VALUES = [data.user_id, data.status, data.user_id, data.status, data.id, data.user_id];
+// 
+//     pool.query(SQLSTATEMENT, VALUES, (error, results) =>
+//     {
+//         if (error) {
+//             return callback(error);
+//         }
+// 
+//         // results[0] is UPDATE OkPacket, results[1] is SELECT rows
+//         const newRep = results[1][0].user_reputation;
+//         callback(null, { affectedRows: results[0].affectedRows, newRep });
+//     });
+// };
